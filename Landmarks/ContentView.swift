8c32cf236d5dd9429397eb59706ded2957c3c993
Benjamin Dirgo <bdirgo@gmail.com>
2020-04-25 12:49:38 -0700

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Benjamin Dirgo on 4/25/20.
+//  Copyright © 2020 Benjamin Dirgo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
